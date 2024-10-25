54d32c087405cb6d5c839ab4e8508002c94e5c1c
Drew Mack <drewmack04@icloud.comm>
2024-10-25 17:20:29 -0400

    update

--- process_data/src/process_vid.py
+++ process_data/src/process_vid.py
@@ -1,6 +1,34 @@
 #use whisper AI and get lectures or videos to break down into text. 
+from moviepy.editor import *
 import whisper
 
-model = whisper.load_model("turbo")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+def MP4toMP3(mp4, mp3):
+    file_mp4 = AudioFileClip(mp4)
+    file_mp4.write_audiofile(mp3)
+    file_mp4.close()
+
+def transcribe(file):
+    model = whisper.load_model("medium.en")
+    try:
+        result = model.transcribe(file)
+        output_path  = "process_data\\output_vid"
+        with open(output_path+"\\output.txt", "w") as f:
+            f.write(result["text"]+"\n")
+        print(result["text"])
+    except Exception as e:
+        print("Error", e)
+
+def main():
+    file_name: str = input("Enter an audiofile: ")
+    input_file = "process_data\\data_vid\\{}".format(file_name)
+    new_input_file = input_file[:-4]+".mp3"
+    if(file_name[:-4]==".mp4"):
+        MP4toMP3(input_file, new_input_file)
+        transcribe(new_input_file)
+        print("complete")
+    else:
+        transcribe(input_file)
+        print("complete")
+
+if __name__=="__main__":
+    main()
