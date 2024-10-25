f99360e1785ac295de113ab76f9223e0077a22c5
Sean Broderick <133047023+seanbrod@users.noreply.github.com>
2024-10-25 17:44:46 -0400

    Update conversion.py

--- process_data/src/conversion.py
+++ process_data/src/conversion.py
@@ -8,12 +8,12 @@ def MP4toMP3(mp4, mp3):
 def main():
     file_name: str = input("Enter an audiofile: ")
     input_file = "process_data\\data_vid\\{}".format(file_name)
-    new_input_file = input_file[:-4]+".mp3"
-    if(file_name[:-4]==".mp4"):
+    new_input_file = ""+input_file[:-4]+".mp3"
+    if(file_name[-4:]==".mp4"):
         MP4toMP3(input_file, new_input_file)
         print("complete")
     else:
-        print("complete")
+        print("complete_null")
 
 if __name__=="__main__":
     main()
